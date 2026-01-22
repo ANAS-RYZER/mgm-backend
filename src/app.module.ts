ebf06583aaf167a4e9e9f2997525f4cc0a4c069a
prathyusha156 <prathyusha@ryzer.app>
2026-01-22 16:26:57 +0530

    admin calls and user calls changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
+import { AdminAuthModule } from './modules/admins/admin-auth.module';
 import { AssetsModule } from './modules/assets/assets.module';
 import { RedisModule } from './infra/redis/redis.module';
 import { MongodbModule } from './infra/mongodb/mongodb.module';
@@ -18,6 +19,7 @@ import { EmailModule } from './infra/email/email.module';
     }),
     AuthModule,
     UsersModule,
+    AdminAuthModule,
     AssetsModule,
     RedisModule,
     MongodbModule,
--- src/modules/admins/dto/verify-otp.dto.ts
+++ src/modules/admins/dto/verify-otp.dto.ts
@@ -1,10 +1,6 @@
 import { IsNotEmpty, IsString, Length } from 'class-validator';
 
 export class VerifyOtpDto {
-  @IsString({ message: 'Token must be a string' })
-  @IsNotEmpty({ message: 'Token is required' })
-  token: string;
-
   @IsString({ message: 'OTP must be a string' })
   @IsNotEmpty({ message: 'OTP is required' })
   @Length(6, 6, { message: 'OTP must be exactly 6 digits' })
