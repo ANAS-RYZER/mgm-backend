5ce67201ab8ea84cae23493dcea1ff365eaa7e63
raju0005 <anderajesh15@gmail.com>
2026-02-12 14:58:31 +0530

    refactor: standardize code formatting and improve readability in appointment and user services

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,14 +1,14 @@
-import { Controller, Get, Request, UseGuards } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { Controller, Get, Request, UseGuards } from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { JwtAuthGuard } from "../auth/guards/jwt-auth.guard";
 
-@Controller('users')
+@Controller("users")
 export class UsersController {
-    constructor(private readonly usersService: UsersService) {}
-    @Get('')
-    @UseGuards(JwtAuthGuard)
-    async getProfile(@Request() req) {
-        const userId = req.user?.userId;
-        return this.usersService.getProfile(userId);
-    }
+  constructor(private readonly usersService: UsersService) {}
+  @Get("/getme")
+  @UseGuards(JwtAuthGuard)
+  async getProfile(@Request() req) {
+    const userId = req.user?.userId;
+    return this.usersService.getProfile(userId);
+  }
 }
